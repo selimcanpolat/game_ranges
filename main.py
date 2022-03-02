19a0e8c9f474637d9da80d26f31916e038d33fac
Selim Can Polat <selimcanpolat1@gmail.com>
2022-03-02 16:24:16 +0300

    Example reset

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from game import Game
 
-poseidon_xtreme = Game(RTP=0.99, variance=10.92618497246)
-ed_jones = Game(0.99, 24.12)
-ed_jones_and_bosx = Game(0.99, 30.1715867)
 
-print(poseidon_xtreme.house_edge())
+example_game = Game(RTP=0.94, variance=10.92)
+
+print(example_game.house_edge(),
+      example_game.max_RTP(1000000),
+      example_game.min_RTP(100000))
 
